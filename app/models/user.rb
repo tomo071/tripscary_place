6305f84ae49f75cd9d2001cd60a0df55bb43b17c
EC2 Default User <tomofun6@gmail.com>
2022-06-21 11:54:15 +0000

    [finish]ポートフォリオ完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :password, presence: true, on: :create
-
+  validates :name, uniqueness: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,5 @@ Rails.application.routes.draw do
     :passwords => 'users/passwords',
     :registrations => 'users/registrations'
   }
-
- 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
